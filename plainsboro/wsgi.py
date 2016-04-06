54ed62b4976f8046cdc77d49d16fe76578327f2d
Vinicius Ribeiro <Vinicius Ribeiro>
2016-04-06 20:22:30 -0300

    staticfiles

--- plainsboro/wsgi.py
+++ plainsboro/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for plainsboro project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
-
 import os
 
 from dj_static import Cling

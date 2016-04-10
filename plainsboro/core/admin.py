747d2661f3bacef011b7cbf0fc803b2fda52e582
Vinicius Ribeiro <Vinicius Ribeiro>
2016-04-10 16:23:32 -0300

    DoctorModelAdmin

--- plainsboro/core/admin.py
+++ plainsboro/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from plainsboro.core.models import Doctor
 
-# Register your models here.
+
+class DoctorModelAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ['name', 'specialization', 'address', 'neighborhood',
+                    'city', 'phone', 'email']
+
+admin.site.register(Doctor, DoctorModelAdmin)
